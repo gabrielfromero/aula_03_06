b4fc9c44e921e1f30565e2b235b002b701a79978
Gabriel <gabrielfaber1@hotmail.com>
2015-06-03 21:04:11 -0300

    Projeto

--- index.php
+++ index.php
@@ -40,18 +40,22 @@
                             <div class="col-sm-offset-2 col-sm-10">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox"> Remember me
+                                        <input type="checkbox" checked> Ativo
                                     </label>
                                 </div>
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="col-sm-offset-2 col-sm-10">
-                                <button type="submit" class="btn btn-default">Sign in</button>
+                                <button type="submit" class="btn btn-success">Cadastrar</button>
                             </div>
                         </div>
                     </form></div>
-                <div class="col-md-6">.col-md-6</div>
+                <div class="col-md-6">.
+                    <h5>
+                        Listagem de Pessoas
+                    </h5>
+                </div>
             </div>
         </div>
 
