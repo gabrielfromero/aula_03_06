a1247cf45ff609f0ef6f09560c398fc20bd69330
Gabriel <gabrielfaber1@hotmail.com>
2015-06-08 21:45:56 -0300

    Aula 08.06

--- index.php
+++ index.php
@@ -1,38 +1,67 @@
+<?php
+
+echo '<pre>';
+var_dump($_GET);
+echo '</pre>';
+
+echo 'teste';
+
+$con = new mysqli('127.0.0.1', 'root', '', 'lp22015');
+
+if (isset($_GET['ativo'])) {
+    $ativo = 1;
+} else {
+    $ativo = 0;
+}
+
+$sql = $con->prepare('INSERT INTO pessoa (nome, endereco, sexo, ativo) VALUES (?,?,?,?);');
+$sql->bind_param('sssi', $_GET['nome'], $_GET['endereco'], $_GET['sexo'], $ativo);
+
+if ($sql->execute()) {
+    echo '<h2>Sucesso<h2>';
+} else {
+    echo '<pre>';
+    var_dump($sql->error_list);
+    echo '</pre>';
+}
+?>
+
+<!DOCTYPE html>
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>Cadastro de pessoas</title>
         <meta charset="ISO-8859-1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
         <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
         <script src="js/jquery-1.11.3.js" type="text/javascript"></script>
-        <script src="js/bootstrap.js" type="text/javascript"></script>
+        <script src="js/bootstrap.js" type="text/javascript"></script>        
     </head>
     <body>
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <form class="form-horizontal">
+                    <form class="form-horizontal" method="GET" action="index.php">
                         <div class="form-group">
                             <label class="col-sm-2 control-label">Nome</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" placeholder="Nome">
+                                <input type="text" name="nome" id="input-nome" class="form-control" placeholder="Nome">
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="col-sm-2 control-label">Endereço</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" placeholder="Endereço">
+                                <input type="text" name="endereco" id="input-endereco" class="form-control" placeholder="Endereço">
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="col-sm-2 control-label">Sexo</label>
                             <div class="col-sm-10">
                                 <label class="radio-inline">
-                                    <input name="genero" type="radio" value="M"> Masculino
+                                    <input name="sexo" type="radio" value="m" id="opt-masc"> Masculino
                                 </label>
                                 <label class="radio-inline">
-                                    <input name="genero" type="radio" value="F"> Feminino
+                                    <input name="sexo" type="radio" value="f" id="opt-fem"> Feminino
                                 </label>
                             </div>
                         </div>
@@ -40,30 +69,41 @@
                             <div class="col-sm-offset-2 col-sm-10">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" checked> Ativo
+                                        <input type="checkbox" name="ativo" checked> Ativo
                                     </label>
                                 </div>
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="col-sm-offset-2 col-sm-10">
-                                <button type="submit" id="btn-enviar" class="btn btn-success">Cadastrar</button>
+                                <button class="btn btn-success" id="btn-enviar" type="submit" >Inserir</button>
                             </div>
                         </div>
-                    </form></div>
-                <div class="col-md-6">.
-                    <h5>
-                        Listagem de Pessoas
-                    </h5>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <h5>Listagem de pessoas</h5>
                 </div>
             </div>
         </div>
         <script>
-            $('#btn-enviar').click(function(){
-                alert('Você clicou no botão')
+            $('#btn-enviar').click(function () {
+                var valido = true;
+                if ($('#input-nome').val() == '') {
+                    valido = false;
+                    alert('Preencha o nome');
+                }
+                if ($('#input-endereco').val() == '') {
+                    valido = false;
+                    alert('Preencha o endereço');
+                }
+                if (!$('#opt-masc').is(':checked') && !$('#opt-fem').is(':checked')) {
+                    valido = false;
+                    alert('Escolha um sexo');
+                }
+
+                return valido;
             })
-            </script>
+        </script>
     </body>
 </html>
-
-
